c1aca53b2b330bda508600dd4a9e0ae747cbe510
limengyi006 <limengyi006@ke.com>
2021-11-15 22:56:00 +0800

    lintcode 562 to summarize
    Signed-off-by: limengyi006 <limengyi006@ke.com>

---
+++ src/com/my4tb/algorithm/lintcode/_562.java
@@ -0,0 +1,24 @@
+package com.my4tb.algorithm.lintcode;
+
+import java.util.Arrays;
+
+public class _562 {
+
+    private static int backPackIV(int[] nums, int target) {
+        int[] f = new int[target + 1];
+        f[0] = 1;
+
+        /*
+            外层循环必须遍历nums
+         */
+        for (int num : nums)
+            for (int j = num; j <= target; j++)
+                f[j] += f[j - num];
+        return f[target];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(backPackIV(new int[]{2, 3, 6, 7}, 7));
+    }
+
+}
